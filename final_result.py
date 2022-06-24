29644276d87f13389d0ec04610c13af8ae429488
tanya-jp <t_djavaherpour@yahoo.com>
2022-06-24 13:03:55 +0430

    final_result completed

--- final_result.py
+++ final_result.py
@@ -1,3 +1,12 @@
+from fuzzification import Fuzzify
+from inference import FuzzyInference
+from defuzzification import Defuzzify
+
+fuzzification_res = Fuzzify()
+inference_res = FuzzyInference()
+defuzzification_res = Defuzzify()
+
+
 class ProvideResult(object):
     def __new__(cls):
         if not hasattr(cls, 'instance'):
@@ -6,4 +15,14 @@ class ProvideResult(object):
 
     @staticmethod
     def get_final_result(input_dict: dict) -> str:
-        pass
+        age, bp, bs, cholesterol, hr, ecg, op, cp, exercise, thallium, sex =\
+            fuzzification_res.fuzzification_result(input_dict)
+
+        fuzzy_sickness = \
+            inference_res.inference_result(age, bp, bs, cholesterol, hr, ecg, op, cp, exercise, thallium, sex)
+
+        sickness_level = defuzzification_res.defuzzification_result(fuzzy_sickness)
+
+        return sickness_level
+
+
